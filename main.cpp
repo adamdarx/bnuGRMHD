83798df69aab560254c3c3117a5739822b4ceb08
Yuehang Li <adamdarx@qq.com>
2025-02-19 16:41:38 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -377,13 +377,13 @@ int main(int argc, char* argv[])
 		fix();
 		
 		totalTime += clock() - start;
-		if (int(totalPhysicalTime) % 10 != int(totalPhysicalTime + Delta_t) % 10)
+		totalPhysicalTime += Delta_t;
+		if(int(totalPhysicalTime - Delta_t) / 10 != int(totalPhysicalTime) / 10)
 		{
 			char filename[16];
 			sprintf(filename, "plt%0.3d", int(totalPhysicalTime) / 10);
 			WriteSingleLevelPlotfile(filename, prim, {"RHO", "UU", "U0", "U1", "U2", "U3", "B1", "B2", "B3", "BSQ"}, geom, totalPhysicalTime, int(totalPhysicalTime) / 10);
 		}
-		totalPhysicalTime += Delta_t;
 		amrex::Print() << "Epoch: " << epoch << "\tTime(ms): " << clock() - start << "\tPhysical Time: " << Delta_t << "\tTotal Physical Time: " << totalPhysicalTime << std::endl;
 	}
 	amrex::Finalize();
